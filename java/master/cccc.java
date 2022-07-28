77faa933d6b98cfe61fddbb5115af497bea771ec
vmaonuooov <110151556+vmaonuooov@users.noreply.github.com>
2022-07-28 16:44:04 +0800

    Add files via upload

---
+++ java/master/cccc.java
@@ -0,0 +1,16 @@
+<<<<<<< HEAD
+
+
+class GoodJava {
+	public 好功能 (){
+		System.out.println("好功能");
+=======
+// 在 好分支 中的注释
+
+class GoodJava {
+	public 好功能 (){
+		System.out.println("好分支中的方法");
+>>>>>>> 好分支
+	}
+}
+
